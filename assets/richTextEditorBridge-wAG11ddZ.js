e477fef00509ac1b44b5b8170d7510faddb5df89
zieweyyyy-code <zieweyyyy@gmail.com>
2026-08-25 10:03:59 +0800

    deploy: publish openim main 4067baf

--- assets/richTextEditorBridge-wAG11ddZ.js
+++ assets/richTextEditorBridge-wAG11ddZ.js
@@ -1,4 +1,4 @@
-import{c as l,j as s,r as k,b as g}from"./index-BEmjMFMU.js";/**
+import{c as l,j as s,r as k,b as g}from"./index-DrG9U00K.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
